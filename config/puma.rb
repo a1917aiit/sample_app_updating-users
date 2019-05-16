5c6f2e42d1acda06ac518ab8118715a94634b60d
a1917ms <a1917ms@aiit.ac.jp>
2019-05-16 10:10:46 +0000

    Use SSL and the Puma webserver in production

--- config/puma.rb
+++ config/puma.rb
@@ -53,7 +53,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 #
 
 # Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
+##plugin :tmp_restart
 
 
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
